99b58d3216ae22f070c7e08ccbf9bdb37e8bdf6f
salman1256@yahoo.com <salman1256@yahoo.com>
2025-08-09 21:48:05 +0530

    Divisible by 5

--- Program.cs
+++ Program.cs
@@ -7,9 +7,14 @@ for (int i = 1; i <= 10; i++)
 {
     System.Console.WriteLine($"{num}*{i}=\t {num * i}");
 }
-
-System.Console.WriteLine("Printing Numbers from 1 to 100");
-for (int i = 1; i <= 100; i++)
-{ 
-    System.Console.Write(i+"\t");
+//1. add the code in program
+//2. We have build and run it
+//3. We have add all & commit the change
+//4. We have push the code
+//5. We will check the jenkins
+System.Console.WriteLine("Printing Numbers from 1 to 500 divisible by 5");
+for (int i = 1; i <=500; i++)
+{
+    if (i % 5 == 0)
+    { System.Console.Write(i + "\t"); }
 }
