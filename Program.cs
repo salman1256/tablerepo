3f808278eab220054f5ccb5dff7ac7dedfdf411a
salman1256@yahoo.com <salman1256@yahoo.com>
2025-08-09 19:43:10 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("We Welcome you in C#");
+int num = 5;
+System.Console.WriteLine($"*Table of {num}*");
+for (int i = 1; i <= 10; i++)
+{
+    System.Console.WriteLine($"{num}*{i}=\t {num * i}");
+}
+
